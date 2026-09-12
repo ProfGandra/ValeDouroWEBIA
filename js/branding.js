ffa15afaf9b501d49b6ba97c8c3aa4954b64cda5
Diego Gandra <tech.gandra@gmail.com>
2026-09-12 00:51:28 -0300

    feat: carrega Como Jogar no menu principal

--- js/branding.js
+++ js/branding.js
@@ -157,10 +157,10 @@
     document.head.appendChild(reputationBridge);
   }
 
-  if(!document.querySelector('script[data-vd-scene-continuity]')){
-    const sceneContinuity=document.createElement('script');
-    sceneContinuity.src='js/scene-continuity.js?v=20260912-1';
-    sceneContinuity.dataset.vdSceneContinuity='1';
-    document.head.appendChild(sceneContinuity);
+  if(!document.querySelector('script[data-vd-how-to-play]')){
+    const howToPlay=document.createElement('script');
+    howToPlay.src='js/how-to-play.js?v=20260912-1';
+    howToPlay.dataset.vdHowToPlay='1';
+    document.head.appendChild(howToPlay);
   }
 })();
